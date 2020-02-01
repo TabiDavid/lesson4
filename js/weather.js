d298936f08356549709f1ca1f5793686d5ef0ef6
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-02-01 15:48:01 -0500

    commit

--- js/weather.js
+++ js/weather.js
@@ -8,8 +8,18 @@ document.querySelector(".navigation").classList.toggle("responsive");
 
 function getUpdate(){
   
-    var d = document.lastModified;
+    var dayOfWeek = new Date().getDay();
+    var month = new Date().getMonth();
+    var dayOfMonth = new Date().getDate();
+    var year = new Date().getFullYear();
+    
+var Months = ['January', 'Febrary', 'March', 'April', 'May', 'Jun', 'July',
+'August', 'September', 'October', 'November', 'December'];
+var weekdays = ['Moday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    document.getElementById("outputDiv").innerHTML = "Last Update: " + d;
-}
 
+    text = weekdays[dayOfWeek] + " , " + dayOfMonth + " " + Months[month] + " " + year;
+    
+    
+    document.getElementById('outputDiv').innerHTML = text;
+    }
