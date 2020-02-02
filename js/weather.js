3633f8de770c5883ff40f5ebc11f72f467bbba93
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-02-01 19:28:58 -0500

    commit

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,4 @@
-//const hambutton = document.querySelector(".ham");
-//hambutton.addEventListener("click", toggleMenu, false);
+
 
 function toggleMenu() {
 document.querySelector(".navigation").classList.toggle("responsive");
